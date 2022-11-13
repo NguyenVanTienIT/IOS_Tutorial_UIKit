28b68849e9f386bc3c45570c9ed68b030e81087c
tien.nguyen23 <tien.nguyen23@mservice.com.vn>
2022-11-13 10:52:19 +0700

    init project

---
+++ IOS_Tutorial_UIKit/IOS_Tutorial_UIKit/TodoListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TodoListViewController.swift
+//  IOS_Tutorial_UIKit
+//
+//  Created by tien.nguyen23 on 08/11/2022.
+//
+
+import Foundation
+import UIKit
+class ToDoListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        // handle when view did load
+    }
+}
--- IOS_Tutorial_UIKit/IOS_Tutorial_UIKit/ViewController.swift
+++ IOS_Tutorial_UIKit/IOS_Tutorial_UIKit/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var aphal: Float?
+    var beta: Float?
+    
+    @IBOutlet weak var result: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func handleTouchOutside(_ sender: UITextField) {
+        
+    }
+    
+    
+    @IBAction func handleTouchOutsideBeta(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func handleOnChangeAphal(_ sender: UITextField) {
+         aphal = Float(sender.text ?? "0")
+         print(sender.text ?? "nil")
+    }
+    
+    
+    @IBAction func handleOnChangeBeta(_ sender: UITextField) {
+         beta = Float(sender.text ?? "0")
+         print(sender.text ?? "nil")
+    }
+    
+    @IBAction func handlePressButton(_ sender: Any) {
+        if aphal != nil && beta != nil {
+            result.text = "This is result \(aphal! * 0.4 + beta! * 0.6)"
+        }
+    }
 }
 
