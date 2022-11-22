a3fa05ee4fff6def743a70f2d4e7b2d12b7ba909
tien.nguyen23 <tien.nguyen23@mservice.com.vn>
2022-11-22 15:13:03 +0700

    add screen

--- IOS_Tutorial_UIKit/IOS_Tutorial_UIKit/TodoListViewController.swift
+++ IOS_Tutorial_UIKit/IOS_Tutorial_UIKit/TodoListViewController.swift
@@ -7,10 +7,39 @@
 
 import Foundation
 import UIKit
-class ToDoListViewController: UIViewController {
+class ToDoListViewController: UIViewController{
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    @IBOutlet weak var todoListView: UICollectionView!
+    var listTodo: [String] = ["AAA", "BBB"]
+    var snapshot = Snapshot()
+    var dataSource: DataSource!
     
     override func viewDidLoad() {
-       setLayoutHeader()
+        setLayoutHeader()
+        
+        // push data to snapshot
+        snapshot.appendSections([0])
+        snapshot.appendItems(listTodo)
+        
+        // make layout for collection view
+        let configLayout = makeLayout()
+        todoListView.collectionViewLayout = configLayout
+        
+        // Register for cell item, map data for another item in list
+        let cellRegister = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIndentifier: String) in
+            let todo = self.listTodo[indexPath.item]
+            var contentConfig = cell.defaultContentConfiguration()
+            contentConfig.text = todo
+            cell.contentConfiguration = contentConfig
+        }
+        dataSource = DataSource(collectionView: todoListView) {(collectionView: UICollectionView, indexPath: IndexPath, item: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegister, for: indexPath, item: item)
+        }
+        dataSource.apply(snapshot)
+        todoListView.dataSource = dataSource
     }
     
     func setLayoutHeader() {
@@ -18,7 +47,39 @@ class ToDoListViewController: UIViewController {
         navigationItem.setRightBarButton(buttonAdd, animated: true)
     }
     
+    
+    // Handle config layout
+    func makeLayout() -> UICollectionViewCompositionalLayout {
+        var listConfig = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfig.backgroundColor = .clear
+        listConfig.showsSeparators = true
+        return UICollectionViewCompositionalLayout.list(using: listConfig)
+    }
+    
     @objc func handleAddItemTodo(sender: UIBarButtonItem) {
-        print("AAA")
+        let dialogAdd = UIAlertController(title: "Add todo", message: "Please enter name todo", preferredStyle: .alert)
+        dialogAdd.addTextField()
+        dialogAdd.textFields?[0].placeholder = "Please enter name"
+        dialogAdd.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
+            action in
+            print(self.listTodo)
+        }))
+        
+        dialogAdd.addAction(UIAlertAction(title: "OK", style: .destructive, handler: {
+            action in
+            let nameTodo = dialogAdd.textFields?[0].text ?? ""
+            if !nameTodo.isEmpty {
+                self.listTodo.append(nameTodo)
+                print(self.listTodo)
+                self.handleReloadListItem()
+            }
+        }))
+        present(dialogAdd, animated: true)
+    }
+    
+    func handleReloadListItem() {
+        DispatchQueue.main.async {
+            self.todoListView.reloadData()
+        }
     }
 }
